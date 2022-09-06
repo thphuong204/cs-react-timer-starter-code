3f885f9b3c5979c9faebc67fa81bb32c7dcde6ae
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-06 20:58:20 +0700

    implement formatTime

--- src/formatTime.js
+++ src/formatTime.js
@@ -1,8 +1,8 @@
 export const formatTime = (time) => {
-  const getSeconds = "Your code here";
-  const minutes = "Your code here";
-  const getMinutes = "Your code here";
-  const getHours = "Your code here";
+  const getHours = Math.floor(time / 3600);
+  const getMinutes = Math.floor((time % 3600) / 60);
+  const getSeconds = time % 3600 % 60;
+
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
